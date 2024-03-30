34f143cd73978fd1f6d7879cd0a45278aa0db368
MinseokGo <rhalstjr1999@naver.com>
2024-03-30 19:03:29 +0900

    feat: front controller v1

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -1,5 +1,8 @@
 package hello.servlet.web.frontcontroller.v1;
 
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,17 +12,28 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerV1")
-public class FrontControllerV1 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
 
     private final Map<String, ControllerV1> controllerV1Map = new HashMap<>();
 
-    public FrontControllerV1() {
-        controllerV1Map.put("")
+    private FrontControllerServletV1() {
+        controllerV1Map.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerV1Map.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerV1Map.put("/front-controller/v1/members", new MemberListControllerV1());
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerV1.service");
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        ControllerV1 controller = controllerV1Map.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        controller.process(request, response);
     }
 }
