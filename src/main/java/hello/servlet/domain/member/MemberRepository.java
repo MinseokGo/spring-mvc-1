e018d70c8cf94f364dc019ad5720b337be65cd71
MinseokGo <rhalstjr1999@naver.com>
2024-04-02 19:50:06 +0900

    feat: find members into mvc

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -9,7 +9,7 @@ import java.util.Map;
 * 해당 Map은 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
 */
 public class MemberRepository {
-    private static Map<Long, Member> store = new HashMap<>();
+    private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
     private static final MemberRepository INSTANCE = new MemberRepository();
--- src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -9,27 +9,19 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
-public class MvcMemberSaveServlet extends HttpServlet {
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
 
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
-
-        Member member = new Member(username, age);
-        memberRepository.save(member);
-
-        // Model 에 데이터를 보관한다.
-        request.setAttribute("member", member);
-
-        // JSP 호출
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
         requestDispatcher.forward(request, response);
     }
