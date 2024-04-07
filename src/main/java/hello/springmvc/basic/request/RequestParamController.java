d88dcf6be8411ec64b2b8bbdde5c35fec4a449ce
MinseokGo <rhalstjr1999@naver.com>
2024-04-07 18:59:28 +0900

    refactor: remove duplicate logic

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,34 +5,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class RequestParamController {
 
-    @GetMapping("/request-param-v1")
-    public void requestParamGetV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @RequestMapping ("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
-        log(username, age);
-
-        response.getWriter()
-                .write("ok");
-    }
-
-    @PostMapping("/request-param-v1")
-    public void requestParamPostV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
-        log(username, age);
-
-        response.getWriter()
-                .write("ok");
-    }
-
-    private void log(String username, int age) {
         log.info("username={}, age={}", username, age);
+
+        response.getWriter()
+                .write("ok");
     }
 }
