1098859b63839615027ee39a472f4830360550bc
MinseokGo <rhalstjr1999@naver.com>
2024-04-09 15:26:47 +0900

    feat: read items form

---
+++ src/main/java/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,40 @@
+package itemservice.web.basic;
+
+import itemservice.domain.item.Item;
+import itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/basic/items")
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(Item.valueOf("itemA", 10000, 10));
+        itemRepository.save(Item.valueOf("itemB", 20000, 20));
+    }
+}
